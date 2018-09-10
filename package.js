189254ef60289631ce02150d4d8cc3691648a826
Bart Sturm <bart@tutormundi.com>
2018-09-10 15:28:21 -0300

    Update description

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'tap:i18n',
-  summary: 'A comprehensive internationalization solution for Meteor',
-  version: '1.8.1',
-  git: 'https://github.com/TAPevents/tap-i18n'
+  name: 'bartoftutormundi:tap-i18n',
+  summary: 'The known and trusted tap-i18n but with Brazilian Portuguese as fallback language',
+  version: '1.8.2',
+  git: 'https://github.com/namenotrequired/tap-i18n'
 });
 
 both = ['server', 'client'];
